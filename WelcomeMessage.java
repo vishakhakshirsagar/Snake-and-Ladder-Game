0b3a12be699eefb1ac098638140d8386011fcc70
vishakhakshirsagar <kshirsagarvishakha22@gmail.com>
2022-12-31 14:30:26 +0530

    Added: The Player rolls the die to get a number between 1 to 6

--- WelcomeMessage.java
+++ WelcomeMessage.java
@@ -1,10 +1,12 @@
 package SnakeAndLadderGame;
-
+import java.util.Random;
 public class WelcomeMessage {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
         int player = 0;// uc1
         System.out.println("Player position => "+player);
-
+        Random random = new Random();
+        int dice = random.nextInt(6)+1;// uc2
     }
 }
