5b062fcce069fb981dce4f15399db4f1a50d18b1
vishakhakshirsagar <kshirsagarvishakha22@gmail.com>
2022-12-31 14:15:55 +0530

    Added: Snake and ladder game start with single player from zero position

---
+++ WelcomeMessage.java
@@ -0,0 +1,10 @@
+package SnakeAndLadderGame;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game");
+        int player = 0;// uc1
+        System.out.println("Player position => "+player);
+
+    }
+}
